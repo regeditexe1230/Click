28fdc00f5ea47a92bd024fc2ccccc84d44f3321b
regeditexe1230 <3394780485@qq.com>
2026-08-26 12:14:20 +0800

    恢复expandView动画原始代码

--- app/src/main/java/com/yjc/click/MainActivity.kt
+++ app/src/main/java/com/yjc/click/MainActivity.kt
@@ -509,12 +509,9 @@ class MainActivity : AppCompatActivity() {
         }
         animator.addListener(object : android.animation.AnimatorListenerAdapter() {
             override fun onAnimationEnd(animation: android.animation.Animator) {
-                // 延迟一帧再恢复 WRAP_CONTENT，避免跳动
-                view.post {
-                    params.height = android.view.ViewGroup.LayoutParams.WRAP_CONTENT
-                    view.layoutParams = params
-                    view.alpha = 1f
-                }
+                params.height = android.view.ViewGroup.LayoutParams.WRAP_CONTENT
+                view.layoutParams = params
+                view.alpha = 1f
             }
         })
         animator.start()
